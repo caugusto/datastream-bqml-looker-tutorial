4ce1efe2ef3565a49aabea0009d76ef94620613a
caugusto <caugusto_ny@yahoo.com>
2022-01-22 12:25:16 -0500

    New date range

--- bigquery_ddl/cr_table_training_data.sql
+++ bigquery_ddl/cr_table_training_data.sql
@@ -6,6 +6,6 @@ product_name,
 SUM(quantity) AS total_sold
    FROM `retail.ORDERS`
        GROUP BY hourly_timestamp, product_name
-       HAVING hourly_timestamp BETWEEN TIMESTAMP_TRUNC('2021-06-01', HOUR) AND TIMESTAMP_TRUNC('2021-11-21', HOUR)
+       HAVING hourly_timestamp BETWEEN TIMESTAMP_TRUNC('2021-11-22', HOUR) AND TIMESTAMP_TRUNC('2021-11-28', HOUR)
        ORDER BY hourly_timestamp
 ;
